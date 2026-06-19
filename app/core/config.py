d5da1e8ae7bf9611c667a7e7ed05434f9d9d1b75
dhrroovv <dhrroovv@gmail.com>
2026-06-19 22:38:47 +0530

    feat: configure jwt

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DB_URL: str = Field(validation_alias="SQL_ALCHEMY_DB_URL")
     LOG_LEVEL: str = "INFO"
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRY: int = 3600
 
     model_config = SettingsConfigDict(env_file=".env")
 
--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,11 @@
+from datetime import datetime, timedelta
+
+import jwt
 from pwdlib import PasswordHash
+
+from app.core.config import get_settings
+
+settings = get_settings()
 
 
 class Passwords:
@@ -11,3 +18,9 @@ class Passwords:
     @classmethod
     def verify_password(cls, password: str, hashed_password: str) -> bool:
         return cls.password_hash.verify(password, hashed_password)
+
+
+class AccessToken:
+    @classmethod
+    def create_access_token(cls, user: dict[str, str], expiry: str | None = None):
+        pass
